86442c88ccfb8503ad7377a1d46472bc57e7daf9
Unknown <157136443@qq.com>
2017-12-30 22:32:48 +0800

    My_Turtle_And_10_Fishes

--- My_Turtle_And_10_Fish.py
+++ My_Turtle_And_10_Fish.py
@@ -1,6 +1,8 @@
 '''
 There is a problem of run out of range when pop
 And do not know why yet!!!
+#It is very dangerous to delete an element from iterator, cause the iterator reference the element to reference directly
+And there is a bug that all the fishes and turtle unexpect to move ti the border finally, so that means there is a bug in the move section 
 '''
 import random
 class Turtle:
@@ -9,39 +11,30 @@ class Turtle:
         self.y = random.randint(0, 10)
         self.endurance = 100
         #self.step = random.randint(1, 2)
+    def __str__(self):
+        return str(self.x) + ',' + str(self.y)
     def move(self):
         flag = random.randint(0, 1)
-        step = random.randint(1, 2)
-        if flag == 0:
-            if step == 1:
-                if self.x >= 10:
-                    self.x -= 1
-                    self.endurance -= 1
-                else:
-                    self.x += 1
-                    self.endurance -= 1
-            elif step == 2:
-                if self.x >= 9:
-                    self.x -= 2
-                    self.endurance -= 1
-                else:
-                    self.x += 2
-                    self.endurance -= 1
-        else:
-            if step == 1:
-                if self.y >= 10:
-                    self.y -= 1
-                    self.endurance -= 1
-                else:
-                    self.y += 1
-                    self.endurance -= 1
-            elif step == 2:
-                if self.y >= 9:
-                    self.y -= 2
-                    self.endurance -= 1
-                else:
-                    self.y += 2
-                    self.endurance -= 1
+        step = random.choice([-2, -1, 1, 2])
+        if flag == 0:                     #flag = 0 means move x
+            new_x = self.x + step
+            self.endurance -= 1
+            if new_x > 10:
+                self.x = 20 - new_x
+            elif new_x < 0:
+                self.x = 0 - new_x
+            else:
+                self.x = new_x
+        else:                             #flag = 1 means move y   
+            self.endurance -= 1                                                             
+            new_y = self.y + step
+            if new_y > 10:
+                self.y = 20 - new_y
+            elif new_y < 0:
+                self.y = 0 - new_y
+            else:
+                self.y = new_y
+        return str(self.x) + ',' +str(self.y)
     def get_Location(self):
         print('乌龟的位置：',self.x, self.y, sep = ' ')   
 
@@ -52,47 +45,51 @@ class Fish:
     def __init__(self):
         self.x = random.randint(0, 10)
         self.y = random.randint(0, 10)
+    def __str__(self):
+        return str(self.x) + ',' + str(self.y)
     def move(self):
         flag = random.randint(0, 1)
-        if flag == 0:
-            if 0 <= self.x < 10:
-                self.x += 1
-            elif self.x >= 10:
-                self.x -= 1
+        step = random.choice([-1, 1])
+        if flag == 0:                     #flag = 0 means move x
+            new_x = self.x + step
+            if new_x > 10:
+                self.x = 20 - new_x
+            elif new_x < 0:
+                self.x = 0 - new_x
             else:
-                self.x += 1
-        else:
-            if 0 <= self.y < 10:
-                self.y += 1
-            elif self.y >= 10:
-                self.y -= 1
+                self.x = new_x
+        else:                            #flag = 1 means move y                                       
+            new_y = self.y + step
+            if new_y > 10:
+                self.y = 20 - new_y
+            elif new_y < 0:
+                self.y = 0 - new_y
             else:
-                self.y += 1
-    def get_Location(self, index):
-        print('鱼%d的位置：' %index, self.x, self.y, sep = ' ')
+                self.y = new_y
+        return str(self.x) + ',' +str(self.y)
+    def get_Location(self):
+        print('鱼的位置：', self.x, ', ' ,self.y,)
 t1 = Turtle()
 t1.get_Location()
 f = []
 for i in range(10):
     f.append(Fish())
-    f[i].get_Location(i)
-i = 40
-while i > 0:
-    index_todelete = []
-    if t1.endurance == 0 or len(f) == 0:
-        print('Game Over!')
+    print(f[i])
+while True:
+    if t1.endurance == 0:
+        print('The Turtle is dead!Game Over!')
         break
-    t1.move()
-    t1.get_Location()
+    if len(f) == 0:
+        print('All the fishes are eaten! Game Over!')
+        break
+    t_pos = t1.move()
     t1.get_Endur()
-    for j in range(len(f)):
-        f[j].move()
-        f[j].get_Location(j)
-        if t1.x == f[j].x and t1.y == f[j].y:
+    for each_fish in f[:]:              #It is very dangerous to delete an element from iterator
+        if t_pos == each_fish.move():   #cause the iterator reference the element to reference directly
             t1.endurance += 20
-            index_todelete.append(j)
-    for index in index_todelete:
-        f.pop(index)
-        print('有一条鱼被吃掉了，呜呜呜呜～～～～～～')
+            f.remove(each_fish)
+            print('有一条鱼被吃掉了，呜呜呜呜～～～～～～')                   
+        
+        
     i -= 1
-print('run out')
+print('Run Out')
